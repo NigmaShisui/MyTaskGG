2d79237ceb95cc576b3d9a321cbff2dcbed0daaa
NigmaShsisui <acpalarca9@gmail.com>
2024-09-28 10:47:56 +0800

    WoW

---
+++ task1.py
@@ -0,0 +1,13 @@
+#print negative, print positive
+ 
+age = [-12,13,-14,15,-16]
+
+for x in age:
+    if x == 13:
+        print("Positive")
+        
+    elif x == 15:
+        print("Positive")
+
+    else:
+        print("Negative")
---
+++ task2.py
@@ -0,0 +1,8 @@
+username = ["Juan","Pedro","James","Sagaad","Fuego"]
+
+for y in username:
+    if y == "James":
+        print("James Cute")
+
+    else:
+        print("James None")
